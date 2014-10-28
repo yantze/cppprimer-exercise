39c5f4727e35fb630dab87eb72cfe39e2103ae42
yantze <yantze@126.com>
2014-10-28 22:13:50 +0800

    organize fold

--- chapter01/examples/1_1.cpp
+++
@@ -1,6 +0,0 @@
-// P5 example 1.1
-//"g++ 1_1.cpp -Wall" to compiler
-
-int main() {
-	return 0; // "echo $?" shows 0
-}
--- chapter01/examples/1_2.cpp
+++
@@ -1,6 +0,0 @@
-// P5 example 1.2
-// use "g++ 1_2.cpp -Wall" to compile
-
-int main() {
-	return -1; // "echo $?" shows 255
-}
